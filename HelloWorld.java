b76812ba88371e731e6c538f96f27ae68318e1c0
fjochum07 <36773495+fjochum07@users.noreply.github.com>
2019-05-21 10:25:08 -0500

    Create HelloWorld.java
    version 1.0 - HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+package fjochumjr;
+
+/**
+ *
+ * @author Frank
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
